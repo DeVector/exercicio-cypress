735695df3078e14c45e00856c8404485c65726f8
DeVector <diniz.vito@gmail.com>
2025-04-14 08:46:58 -0300

    Exercicio com testes

---
+++ cypress/e2e/exercicio-cypress/agenda_de_contatos.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Testando a agenda de contatos', () => {
+
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+
+    it('Testando a funcionalidade de insercao de contato', () => {
+        cy.get('input[type="text"]').type('Victor Oliveira')
+        cy.get('input[type="email"]').type('victoroliveira@email.com')
+        cy.get('input[type="tel"]').type('83123456789')
+        cy.get('.adicionar').click()
+    })
+
+    it('Testando a funcionalidade de alteração de contatos', () => {
+        cy.get(':nth-child(4) > .sc-gueYoa > .edit').click()
+        cy.get('input[type="text"]').clear().type('Victor Bezerra')
+        cy.get('input[type="email"]').clear().type('victorbezerra@email.com')
+        cy.get('.alterar').click()
+    })
+
+    it('Testando a funcionalidade de remoção de contato', () => {
+        cy.get(':nth-child(4) > .sc-gueYoa > .delete').click()
+    })
+})
